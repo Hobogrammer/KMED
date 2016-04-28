52e7b9af06cf0e7e11246e6b004bdc41e5e79cdb
Hobogrammer <andrepsmithjr@gmail.com>
2016-04-27 20:38:28 -0700

    Start putting it together with classes

--- lib/kmed.rb
+++ lib/kmed.rb
@@ -1,56 +1,39 @@
 require 'csv'
-require 'java'
-require './jars/eb4j-core-1.0.5.jar'
-require './jars/commons-collections-3.2.1.jar'
-require './jars/commons-cli-1.2.jar'
-require './jars/commons-io-1.4.jar'
-require './jars/commons-lang-2.4.jar'
-require './jars/conv-wdic-1.0.5.jar'
-require './jars/conv-zipcode-1.0.5.jar'
-require './jars/eb4j-tools-1.0.5.jar'
-require './jars/log4j-1.2.14.jar'
-require './jars/slf4j-api-1.5.10.jar'
-require './jars/slf4j-log4j12-1.5.10.jar'
-require './jars/xml2eb-1.0.5.jar'
+require_relative 'dictionary'
+require_relative 'gaiji'
 
-java_import 'fuku.eb4j.Book'
-java_import 'fuku.eb4j.hook.DefaultHook'
 
-def unarray(deep_array)
-  puts "INSPECT ARRAY: #{deep_array}"
-  deep_array.empty? ? deep_array : deep_array[0].join(' ')
-end
+def main
+  dic_path = ARGV[0]
+  kindle_export_path = ARGV[1]
 
-timestamp = Time.now.strftime("%Y%m%d%H%M%S")
-book_file = File.open("resource/JMdict")
-book = Book.new(book_file.path)
-book_file.close
-sub_dic = book.getSubBooks
-dic = sub_dic[0]
-hook = DefaultHook.new(dic)
+  timestamp = Time.now.strftime("%Y%m%d%H%M%S")
 
-CSV.open("./resource/#{timestamp}_ankiImport.csv", "w") do |csv|
-  File.foreach("./resource/kindleExportTest.txt") do |line|
-    parts = line.scan(/^(\S*)[\s](\S*)/)
-    term = parts[0][0].gsub(/(\(.*?\))/, '')
-    definition = String.new
+  puts "Dictionary File: #{dic_path}"
+  puts "Kindle export path: #{kindle_export_path}"
 
-    if !term.empty?
-      searcher = dic.searchWord(term)
-      result = searcher.getNextResult
-      definition = result.getText(hook) if result
-    end
+  dic = Dictionary.new(dic_path)
+  CSV.open("./resource/#{timestamp}_ankiImport.csv", "w") do |csv|
+    File.foreach(kindle_export_path) do |line|
+      parts = line.scan(/^(\S*)[\s](\S*)/)
+      term = parts[0][0].gsub(/(\(.*?\))/, '')
+      definition = dic.search(term)
+
       
-    puts "Captured Term: #{term}"
-    puts "Searched Definition: #{definition}"
-    puts "Unfiltered line: #{parts}"
-    parts = unarray(parts)
-    puts "Whole line: #{parts}"
-    furi = parts.scan(/(\(.*?\))/)
-    furi = unarray(furi)
-    puts "Captured furigana: #{furi}"
-    furi.empty? ? (puts "No Furi") : pure_furi = furi.gsub(/\(|\)/, '')
-    puts "Pure Furi: #{pure_furi}"
-    csv << ["#{term}","#{furi}","#{definition}","#{parts}"]
+      puts "Captured Term: #{term}"
+      puts "Searched Definition: #{definition}"
+      puts "Unfiltered line: #{parts}"
+      parts = parts.flatten.join(' ') #seperate the desentence and furigana before this for easier processing
+      puts "PARTS AFTER FLATTEN: #{parts}"
+      puts "Whole line: #{parts}"
+      furi = parts.scan(/(\(.*?\))/)
+      furi = furi.flatten.join(' ')
+      puts "Captured furigana: #{furi}"
+      furi.empty? ? (puts "No Furi") : pure_furi = furi.gsub(/\(|\)/, '')
+      puts "Pure Furi: #{pure_furi}"
+      csv << ["#{term}","#{furi}","#{definition}","#{parts}"]
+    end
   end
 end
+
+main()
