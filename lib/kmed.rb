ab6942201a35fab89396eb625bca9d59edfc7f44
Hobogrammer <andrepsmithjr@gmail.com>
2016-05-02 23:32:25 -0700

    Add gaiji class and degaijify definition

--- lib/kmed.rb
+++ lib/kmed.rb
@@ -9,20 +9,15 @@ def main
 
   timestamp = Time.now.strftime("%Y%m%d%H%M%S")
 
-  puts "Dictionary File: #{dic_path}"
-  puts "Kindle export path: #{kindle_export_path}"
-
   dic = Dictionary.new(dic_path)
+  gaiji = Gaiji.new(dic.name.upcase)
   CSV.open("./resource/#{timestamp}_ankiImport.csv", "w") do |csv|
     File.foreach(kindle_export_path) do |line|
       term_w_furi, sentence = line.match(/^(\S*)[\s](\S*)/).captures
       term = term_w_furi.gsub(/(\(.*?\))/, '')
       definition = dic.search(term)
 
-      
-      puts "Captured Term: #{term}"
-      puts "Searched Definition: #{definition}"
-      puts "Sentence: #{sentence}"
+      definition = gaiji.de_gaiji(definition)
       csv << ["#{term}","#{term_w_furi}","#{definition}","#{sentence}"]
     end
   end
