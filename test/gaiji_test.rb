8c97b52750bc57dd611f60ab59d200c042002e41
Hobogrammer <andrepsmithjr@gmail.com>
2016-05-25 22:37:50 -0700

    Add tests for Gaiji

--- test/gaiji_test.rb
+++ test/gaiji_test.rb
@@ -3,49 +3,48 @@ require 'gaiji'
 
 describe "Gaiji" do
 
-  describe "should find gaiji codes in definitions and replace them with the unicode counter part" do
+  describe "should replace gaiji codes in strings and replace them with the unicode counter part" do
 
     it "for Chujiten" do
-      code_definition = ""
-      gaiji = Gaiji.new("CHIJITEN")
-      assert_equal("", gaiji.de_gaiji(code_sentence))
+      gaiji_string = "[GAIJI=wA37D]"
+      gaiji = Gaiji.new("CHUJITEN")
+      assert_equal("®", gaiji.de_gaiji(gaiji_string))
     end
 
     it "for Daijirin" do
-      code_definition = "つぐ [1] 【[GAIJI=wA740]榔・[GAIJI=wA740]榔子】"
+      gaiji_string = "[GAIJI=wA740]"
       gaiji = Gaiji.new("DAIJIRIN")
-      assert_equal("", gaiji.de_gaiji(code_sentence))
+      assert_equal("桄", gaiji.de_gaiji(gaiji_string))
     end
 
     it "for Daijisen" do
-      code_definition = "い‐さん【遺産】ヰ‐
-      [GAIJI=wC373]死後に残した財産。法律的には、人が死亡当時持っていた所有権・債権・債務も含む全財産をいう。相続財産。[GAIJI=wC374]前代の人が残した業績。「文化—」"
+      gaiji_string = "[GAIJI=wC373]死後に残した財産"
       gaiji = Gaiji.new("DAIJISEN")
-      assert_equal("", gaiji.de_gaiji(code_sentence))
+      assert_equal("①死後に残した財産", gaiji.de_gaiji(gaiji_string))
     end
 
-    it "for Genius" do
-      code_definition = ""
-      gaiji = Gaiji.new("GENIUS")
-      assert_equal("", gaiji.de_gaiji(code_sentence))
-    end
+    #it "for Genius" do
+      #gaiji_string = ""
+      #gaiji = Gaiji.new("GENIUS")
+      #assert_equal("", gaiji.de_gaiji(gaiji_string))
+    #end
 
     it "for Kojien" do
-      code_definition = ""
+      gaiji_string = "[GAIJI=wA456]"
       gaiji = Gaiji.new("KOJIEN")
-      assert_equal("", gaiji.de_gaiji(code_sentence))
+      assert_equal("噦", gaiji.de_gaiji(gaiji_string))
     end
 
     it "for Meikyojj" do
-      code_definition = ""
+      gaiji_string = "[GAIJI=wA36D]"
       gaiji = Gaiji.new("MEIKYOJJ")
-      assert_equal("", gaiji.de_gaiji(code_sentence))
+      assert_equal("Ⅲ", gaiji.de_gaiji(gaiji_string))
     end
 
     it "for Meikyou" do
-      code_definition = "ぎょう‐せき【業績】ゲフ─[GAIJI=wB03E]名[GAIJI=wB03F]"
+      gaiji_string = "[GAIJI=wA17A][GAIJI=wA178]"
       gaiji = Gaiji.new("MEIKYOU")
-      assert_equal("", gaiji.de_gaiji(code_sentence))
+      assert_equal("㋘㋖", gaiji.de_gaiji(gaiji_string))
     end
   end
 end
