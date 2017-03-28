9c5ecd5b0e19bc35d897d932ff7a218c9180b942
Hobogrammer <andrepsmithjr@gmail.com>
2017-03-27 19:46:37 -0700

    Add testing

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ ruby '2.2.2', :engine => 'jruby', :engine_version => '9.0.4.0'
 
 gem 'rake'
 gem 'pry-nav'
+gem 'simplecov', :require => false, :group => :test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,9 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test/test_'
+  add_filter '.jbundler'
+  refuse_coverage_drop
+end
+
 require 'minitest/autorun'
 require 'minitest/spec'
